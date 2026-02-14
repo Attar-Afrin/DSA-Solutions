e3082e5e9e348c0c408236b92a66246b771e1881
Attar Afrin <attarafrin503@gmail.com>
2026-02-14 08:12:06 +0530

    Time: 38 ms (80.28%), Space: 19.3 MB (49.6%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res=0
+        flag=1
+            
+        if x<0:
+            flag=0
+                
+            x=x+(2*(-x))         
+        while x>0:
+            r=x%10
+            res=res*10+r
+            x//=10
+        if not flag:
+            res=-res
+        if res<-2**31 or res>2**31-1:
+            return 0
+        return res
+
+        
